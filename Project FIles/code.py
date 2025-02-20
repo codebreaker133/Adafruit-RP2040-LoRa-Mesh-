a4aa46d4f57cbedf27b1ec133a4bfccb7f2fc051
codebreaker133 <98831118+codebreaker133@users.noreply.github.com>
2025-02-20 10:06:59 -0600

    removing button due to exesive error will circle back to this feature later

--- Project FIles/code.py
+++ Project FIles/code.py
@@ -14,13 +14,16 @@ term = terminal
 Term_open = True
 import board, digitalio
 # import displaydriver1327
-
+import neopx
+neo = neopx
+neo.blink_neo_color(255, 255, 255)
+# boot_button = digitalio.digitalinout(board.button)
+# boot_button.switch_to_input(pull=digitalio.Pull.UP)
 def terminit(Term_open):
     reconfig = False
     while Term_open == True:
         Term_open, reconfig = term.terminal()
-        boot_button = digitalio.digitalinout(board.BOOT)
-        boot_button.switch_to_input(pull=digitalio.Pull.UP)
+        
         if reconfig == True:
             
             NODE, FREQ, tx_power = conedit.varinit()
@@ -28,9 +31,11 @@ def terminit(Term_open):
             
             # Dd = displaydriver1327
             # Dd.write_to_display("reconfigureing...")
-        if boot_button.value:
-            import neopx
-            neopx.blink_neo_white()
+        
+        # if boot_button.value ==True:
+        #     print("button pressed")
+        #     import neopx
+        #     neopx.blink_neo_color(255,255,255)
 
 
 terminit(Term_open) # coment this line out if the terminal is not used, see terminal program for use cases
--- Project FIles/neopx.py
+++ Project FIles/neopx.py
@@ -2,13 +2,14 @@ import time
 import board
 import neopixel
 
-def blink_neo_white():
+def blink_neo_color(r,g,b):
     led = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
-    led.brightness = 0.3
+    led.brightness = 1
+    print("makeing neopx blink")
 
     while True:
-        led[0] = (255, 255, 255)
+        led[0] = (r, g, b)
         time.sleep(1)
         led[0] = (0,0,0)
         break
