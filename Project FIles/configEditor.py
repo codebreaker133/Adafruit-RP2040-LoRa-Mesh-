63655a67aee7aa7063fd7e0f85c1483156a8d707
codebreaker133 <98831118+codebreaker133@users.noreply.github.com>
2025-03-31 14:42:20 -0500

    updating cleaning up and updating main code for radio
    just removed some repetitive code and updated references to RFM library

--- Project FIles/configEditor.py
+++ Project FIles/configEditor.py
@@ -8,4 +8,6 @@ def varinit(): #returns initial values needed for startup of Radio Module
     NODE = fm.configread(filename,"Node")
     FREQ = fm.configread(filename,"freq")
     tx_power = fm.configread(filename,"tx_power")
-    return NODE, FREQ, tx_power
+    spread_factor = fm.configread(filename,"spread_factor")
+    codeing_rate = fm.configread(filename, "coding_rate")
+    return NODE, FREQ, tx_power, spread_factor, codeing_rate
